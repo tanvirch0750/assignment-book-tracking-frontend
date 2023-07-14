f33990f23f538752958d6a9da43c2686a5b5e0f4
tanvirch0750 <tanvir0750@yahoo.com>
2023-07-15 01:22:33 +0600

    implemented search and select component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
-      <p>Book Tracker</p>
-      <input type="text" />
+      <Link to="/" className="font-medium tracking-widest">
+        Book Tracker
+      </Link>
+
       <div>
         <button>sign in</button>
         <button>sign up</button>
--- src/pages/AllBooks.tsx
+++ src/pages/AllBooks.tsx
@@ -1,5 +1,15 @@
+import Filter from '../components/Filter';
+import SearchBook from '../components/SearchBook';
+
 function AllBooks() {
-  return <div>All Books</div>;
+  return (
+    <main className="px-2 py-4">
+      <div className="flex items-center justify-between border-b pb-4">
+        <SearchBook />
+        <Filter />
+      </div>
+    </main>
+  );
 }
 
 export default AllBooks;
