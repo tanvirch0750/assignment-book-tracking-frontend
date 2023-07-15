f51567066348577c7d62cc0fa1451505ce4335e3
tanvirch0750 <tanvir0750@yahoo.com>
2023-07-15 16:23:37 +0600

    implemented home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,20 @@
+import BookLIst from '../components/BookLIst';
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <main className="px-2 py-4">
+      <div className="rounded-md bg-yellow-50 p-8">
+        <blockquote className="text-2xl font-bold text-stone-600">
+          "Reality doesn’t always give us the life that we desire, but we can
+          always find what we desire between the pages of books."
+        </blockquote>
+        <span className="mt-2 block text-base font-medium italic text-stone-700">
+          -- Adelise M. Cullens
+        </span>
+      </div>
+      <BookLIst />
+    </main>
+  );
 }
 
 export default Home;
